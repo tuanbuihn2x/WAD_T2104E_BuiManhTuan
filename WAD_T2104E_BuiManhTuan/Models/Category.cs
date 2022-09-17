879efb787f2565b954ef09f7a9c171de5a84e86e
Manh Tuan Bui <tuanbuihn2x@gmail.com>
2022-09-17 21:04:46 +0700

    update

--- WAD_T2104E_BuiManhTuan/Models/Category.cs
+++ WAD_T2104E_BuiManhTuan/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,8 @@ namespace WAD_T2104E_BuiManhTuan.Models
     public class Category
     {
         public int CategoryId { get; set; }
-        
+
+        [Required(ErrorMessage = "Please enter your Category Name.")]
         public string CategoryName { get; set; }
 
         public ICollection<Product> Products { get; set; } 
--- WAD_T2104E_BuiManhTuan/Models/Product.cs
+++ WAD_T2104E_BuiManhTuan/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +10,18 @@ namespace WAD_T2104E_BuiManhTuan.Models
     {
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = "Please enter Name.")]
+        [StringLength(32, MinimumLength = 3)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter Price.")]
         public int Price { get; set; }
 
+        [Required(ErrorMessage = "Please enter Quantity.")]
+        [Range(1, 100, ErrorMessage = "Value must be between 1 to 100")]
         public int Quantity {get; set; }
 
+        [Required(ErrorMessage = "Please enter Release Date.")]
         public DateTime ReleaseDate { get; set; }
 
         public int CategoryId { get; set; }
